15487f6174911ce8818c2fea05ec110b650e21f7
NataleMarcos <marcosjaviernatale@gmail.com>
2024-09-21 18:05:26 -0300

    avances

---
+++ src/main/java/com/marquitos/pizzeria/web/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.marquitos.pizzeria.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+
+public class SecurityConfig { 
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+		http
+			.csrf().disable()
+			.authorizeRequests()
+			.anyRequest()
+			.authenticated()
+			.and()
+			.httpBasic();
+
+
+
+
+		return http.build();
+
+	}
+
+}
